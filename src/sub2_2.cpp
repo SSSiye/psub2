a534598fd6bb6dfa0a72a959be78c8d8c18d2041
siye <134616474+SSSiye@users.noreply.github.com>
2025-04-20 21:59:20 +0900

    Create sub2_2.cpp

---
+++ src/sub2_2.cpp
@@ -0,0 +1,12 @@
+#include "psub2/sub2_2.hpp"
+
+void Sub::subscribe_msg(const geometry_msgs::msg::Vector3::SharedPtr msg) const
+{
+    RCLCPP_INFO(this->get_logger(), "Received message: x=%.2f, y=%.2f, z=%.2f", msg->x, msg->y, msg->z);
+}
+
+Sub::Sub() : Node("mysub")
+{
+    auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10));
+    sub_ = this->create_subscription<geometry_msgs::msg::Vector3>("mytopic", qos_profile, std::bind(&Sub::subscribe_msg, this, _1));
+}
